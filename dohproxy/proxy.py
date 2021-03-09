9b933ca9371b38dcb2fbd7f8bcc289562812ab56
Mathias Ronimus <mathias@safesurfer.co.nz>
2021-03-10 10:34:25 +1300

    Fix invalid reference to request in proxy

--- dohproxy/proxy.py
+++ dohproxy/proxy.py
@@ -149,7 +149,7 @@ class H2Protocol(asyncio.Protocol):
             self.return_400(stream_id, body=e.body())
             return
 
-        clientip = utils.get_client_ip(request.transport)
+        clientip = utils.get_client_ip(self.transport)
         self.logger.info(
             '[HTTPS] {} {}'.format(
                 clientip,
@@ -185,7 +185,7 @@ class H2Protocol(asyncio.Protocol):
                 ('cache-control', 'max-age={}'.format(ttl))
             )
 
-        clientip = utils.get_client_ip(request.transport)
+        clientip = utils.get_client_ip(self.transport)
         interval = int((time.time() - self.time_stamp) * 1000)
         self.logger.info(
             '[HTTPS] {} {} {}ms'.format(
@@ -205,7 +205,7 @@ class H2Protocol(asyncio.Protocol):
         self.transport.write(self.conn.data_to_send())
 
     async def resolve(self, dnsq, stream_id):
-        clientip = utils.get_client_ip(request.transport)
+        clientip = utils.get_client_ip(self.transport)
         dnsclient = DNSClient(self.upstream_resolver, self.upstream_port,
                               logger=self.logger)
         dnsr = await dnsclient.query(dnsq, clientip)
@@ -272,7 +272,7 @@ class H2Protocol(asyncio.Protocol):
             stream_data = self.stream_data[stream_id]
         except KeyError:
             # Unknown stream, log and ignore (the stream may already be ended)
-            clientip = utils.get_client_ip(request.transport)
+            clientip = utils.get_client_ip(self.transport)
             self.logger.info(
                 '[HTTPS] %s Unknown stream %d', clientip, stream_id
             )
